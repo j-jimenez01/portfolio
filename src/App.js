991e6b1f5f7fa17293423abe3caccc92f5dd404f
NoMamesWey1 <josejimenez530@yahoo.com>
2023-08-03 23:30:12 -0700

    new design
    Cooler design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import Navbar from "./Navbar"
-import Projects from "./pages/Projects"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Resume from "./pages/Resume"
-import { Route, Routes } from "react-router-dom"
+import './App.scss';
+import { Routes,Route } from 'react-router-dom';
+import Layout from './components/Layout';
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
