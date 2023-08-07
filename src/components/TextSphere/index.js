f5bd0ceacf3c061ec86da3bd81c17d960ca90ff5
NoMamesWey1 <josejimenez530@yahoo.com>
2023-08-07 13:36:30 -0700

    updating

--- src/components/TextSphere/index.js
+++ src/components/TextSphere/index.js
@@ -1,55 +1,3 @@
-// import { useEffect } from "react";
-// import './index.scss';
-// import TagCloud from "TagCloud";
-
-// const TextSphere =() => {
-//     useEffect(() => {
-//         return () => {
-//             const container = '.tagcloud'
-//             const texts = [
-//                 "HTML",
-//                 "CSS",
-//                 "Python",
-//                 "JavaScript",
-//                 "React",
-//                 "Git",
-//                 "Github",
-//                 "C",
-//                 "C++",
-//                 "C#",
-//                 "SQL",
-//                 "PostgreSQL",
-//                 "MySQL",
-//                 "MongoDB",
-//                 "npm"
-
-//             ];
-
-//             const options= {
-//                 radius: 300,
-//                 maxSpeed: "normal",
-//                 initSpeed: "normal",
-//                 keep: true,
-//             };
-
-//             TagCloud(container,texts,options);
-//         };
-//     },[]);
-
-//     return (
-//         <>
-//             <div className="text-sphere">
-//                 <span className="tagcloud"></span>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default TextSphere
-
-
-
-
 import React, { useEffect } from "react";
 import './index.scss';
 import TagCloud from "TagCloud"; // Check if this import is correct
@@ -77,8 +25,8 @@ const TextSphere = () => {
 
         const options = {
             radius: 300,
-            maxSpeed: "normal",
-            initSpeed: "normal",
+            maxSpeed: "fast",
+            initSpeed: "fast",
             keep: true,
         };
 
